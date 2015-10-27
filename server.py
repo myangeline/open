e3ac5855e655ad117aa2829a6dc7f5ff53c01a05
Administrator <269614597@qq.com>
2015-10-27 22:35:30 +0800

    add lots of files

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import web
 
 from web.contrib.template import render_jinja
@@ -7,7 +8,12 @@ __author__ = 'sunshine'
 
 urls = (
     '/', 'Index',
-    '/manage', 'ManageCategory',
+    '/login', 'Login',
+    '/manage', 'ManageBlog',
+    '/manage/blog', 'ManageBlog',
+    '/manage/category', 'ManageCategory',
+    '/category/(\d+)', 'Category',
+    '/blog/(\d+)', 'Blog'
 )
 
 app = web.application(urls, locals())
@@ -16,14 +22,48 @@ render = render_jinja('templates', encoding='utf-8')
 
 
 class Index:
+    """
+    首页
+    """
     def GET(self):
-        name = "lujin"
         return render.index(locals())
 
 
+class Login:
+    """
+    登录页
+    """
+    def GET(self):
+        return render.login(locals())
+
+
 class ManageCategory:
+    """
+    类别管理页
+    """
     def GET(self):
         return render.manage_category(locals())
+
+
+class ManageBlog:
+    """
+    博客管理页
+    """
+    def GET(self):
+        return render.manage_blog(locals())
+
+
+class Category:
+    def GET(self, category_id):
+        print('category_id:', category_id)
+        return render.category(locals())
+
+
+class Blog:
+    def GET(self, blog_id):
+        print("blog:", blog_id)
+        return render.blog(locals())
+
 
 
 if __name__ == '__main__':
