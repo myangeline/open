68bd195bd059c08184f408f48f6cec4fb3c7e63b
Administrator <269614597@qq.com>
2015-10-31 22:24:31 +0800

    modify some html

--- conf/settings.py
+++ conf/settings.py
@@ -5,9 +5,11 @@ debug = True
 
 store_path = 'static/files/'
 
-host = '192.168.0.62'
+# host = '192.168.0.62'
+host = '127.0.0.1'
 db = 'ebdb_awesome'
 user = 'root'
-password = '53iq.com'
+# password = '53iq.com'
+password = ''
 charset = 'utf8mb4'
 
